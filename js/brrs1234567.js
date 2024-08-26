e072ba6f2fce0fd5b59caa6f4a78312c85140044
luisavi244 <lavilezc@fafi.utb.edu.ec>
2024-08-26 17:19:59 -0500

    update

--- js/brrs1234567.js
+++ js/brrs1234567.js
@@ -1,43 +1,26 @@
-// script.js
-
-// Variable para controlar el estado de pago
 let pagado = false;
 
-// Función para activar el efecto borroso
-function activarBlurr() {
-    if (!pagado) {
-        document.body.classList.add('blurr');
+function actualizarEstado() {
+    const elementoPendiente = document.querySelector('.pendiente');
+    if (pagado) {
+        document.body.style.overflow = "auto";
+        document.body.style.position = "static";
+        elementoPendiente.style.display = 'none';
+    } else {
+        document.body.style.overflow = "hidden";
+        document.body.style.position = "fixed";
+        elementoPendiente.style.display = 'flex';
     }
 }
 
-// Función para desactivar el efecto borroso cuando se realiza el pago
-function confirmarPago() {
-    pagado = true;
-    document.body.classList.remove('blurr');
+function verificarEstadoPago() {
+    fetch('estado-pago.json')
+        .then(response => response.json())
+        .then(data => {
+            pagado = data.pagado;
+            actualizarEstado();
+        })
+        .catch(error => console.error('Error al cargar el estado de pago:', error));
 }
 
-// Aplicar el efecto borroso al cargar la página
-window.onload = function() {
-    activarBlurr();
-    
-    // Configurar un observador de mutaciones para monitorear cambios en la clase del body
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (!document.body.classList.contains('blurr') && !pagado) {
-                activarBlurr(); // Re-aplicar el efecto si alguien intenta desactivarlo y no se ha pagado
-            }
-        });
-    });
-
-    // Configuración del observador
-    const config = { attributes: true, attributeFilter: ['class'] };
-
-    // Iniciar la observación
-    observer.observe(document.body, config);
-};
-
-// Esta función se llamaría cuando recibas el pago
-function realizarPago() {
-    confirmarPago();
-    // Aquí podrías añadir cualquier otra acción que quieras realizar después del pago
-}
+window.onload = verificarEstadoPago;
