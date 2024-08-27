5a358ba2ecfe6cf47ec21ea3845be05f4070e741
luisavi244 <lavilezc@fafi.utb.edu.ec>
2024-08-27 15:24:13 -0500

    efectoscroll

---
+++ index.js
@@ -0,0 +1,92 @@
+window.sr = ScrollReveal();
+
+    sr.reveal(".introduccion",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".redaccion_culinarias",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".galeria",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+    sr.reveal(".galeria_container",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".cantidad",{
+        duration : 7000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".opnion",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".titulo_video",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".videos-container",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    sr.reveal(".banner",{
+        duration : 3000,
+        origin : "bottom",
+        distance : "100px"
+    });
+    
+    sr.reveal(".card_plato",{
+        duration : 2000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    
+    sr.reveal(".banneer",{
+        duration : 2000,
+        origin : "bottom",
+        distance : "100px"
+    });
+    
+    sr.reveal(".lugar_card",{
+        duration : 2000,
+        origin : "bottom",
+        distance : "100px"
+    });
+
+    
+    sr.reveal(".historia_titulo",{
+        duration : 2000,
+        origin : "bottom",
+        distance : "100px"
+    });
+    
+    sr.reveal(".historia_subtitulo",{
+        duration : 2000,
+        origin : "bottom",
+        distance : "100px"
+    });
+    
+    sr.reveal(".historia_parrafo",{
+        duration : 2000,
+        origin : "bottom",
+        distance : "100px"
+    });
